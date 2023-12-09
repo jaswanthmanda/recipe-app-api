c1b5936b413792d765155ece8089c816e9fc6c91
Jaswanth Manda <jaswanthmandaspd@gmail.com>
2023-12-10 00:11:52 +0530

    added db service

---
+++ app/app/calc.py
@@ -0,0 +1,26 @@
+"""Calculator functions
+"""
+
+def add(x, y):
+    """_summary_
+
+    Args:
+        x (_type_): _description_
+        y (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return x + y
+
+def subtract(x, y):
+    """Subtract
+
+    Args:
+        x (_type_): _description_
+        y (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return x-y
---
+++ app/app/tests.py
@@ -0,0 +1,26 @@
+"""Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the mod
+
+    Args:
+        SimpleTestCase (_type_): _description_
+    """
+    
+    def test_add_number(self):
+        """Test adding numbers tog"""
+        
+        res = calc.add(5, 6)
+        
+        self.assertEqual(res, 11)
+        
+    def test_subtract(self):
+        
+        res = calc.subtract(20, 15)
+        
+        self.assertEqual(res, 5)
