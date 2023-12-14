fa19eb6507c0b4e12075a1f3245ada4774b4e7d6
Jaswanth Manda <jaswanthmandaspd@gmail.com>
2023-12-14 09:06:52 +0530

    added upto user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,37 @@
-from django.db import models # noqa
+"""
+Database models
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+    """Manager for user
+
+    Args:
+        BaseUserManager (_type_): _description_
+    """
+
+    def create_user(self, email, password=None, **extra_field):
+        """Create, save and return a new user."""
+        user = self.model(email=email, **extra_field)
+        user.set_password(password)
+        user.save(using=self._db)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
