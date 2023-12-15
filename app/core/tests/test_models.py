4a9b9f05ece5382691b9b527b5f59fe4576cf4ff
Jaswanth Manda <jaswanthmandaspd@gmail.com>
2023-12-15 08:30:36 +0530

    fixed a typo in testing the exp

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -36,5 +36,5 @@ class ModelTests(TestCase):
 
     def test_new_user_without_email_raises_error(self):
         """Test that creating a user without an email raises a ValueError"""
-        with self.assetRaises(ValueError):
+        with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
