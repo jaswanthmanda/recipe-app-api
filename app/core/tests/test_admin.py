306910a2f38876055e8774bf31712084a86c6268
Jaswanth Manda <jaswanthmandaspd@gmail.com>
2023-12-19 08:45:20 +0530

    fixed a typo

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -33,7 +33,7 @@ class AdminSiteTests(TestCase):
 
     def test_edit_user_page(self):
         """Test edit user page works"""
-        url = reverse('admin:core_user_chage', args=[self.user.id])
+        url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
