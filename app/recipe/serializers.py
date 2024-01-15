30cb3bc8fbf55c791844e0523b45aeae545a3aa0
Jaswanth Manda <jaswanthmandaspd@gmail.com>
2024-01-15 06:27:21 +0530

    upto ingredients api (st)

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -63,24 +63,21 @@ class RecipeSerializer(serializers.ModelSerializer):
         tags = validated_data.pop('tags', [])
         ingredients = validated_data.pop('ingredients', [])
         recipe = Recipe.objects.create(**validated_data)
-        # auth_user = self.context['request'].user
         self._get_or_create_tags(tags, recipe)
         self._get_or_create_ingredients(ingredients, recipe)
-        # for tag in tags:
-        #     tag_obj, created = Tag.objects.get_or_create(
-        #         user=auth_user,
-        #         **tag,
-        #     )
-        #     recipe.tags.add(tag_obj)
-
         return recipe
 
     def update(self, instance, validated_data):
         """Update a recipe."""
         tags = validated_data.pop('tags', [])
+        ingredients = validated_data.pop('ingredients', [])
         if tags is not None:
             instance.tags.clear()
             self._get_or_create_tags(tags, instance)
+
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
